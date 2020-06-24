433a7a50e1080cac0984125ea3cad7e32853dc9f
caodanh180296 <caodanh1802@yahoo.com.vn>
2020-06-24 21:51:34 +0700

    Updates

---
+++ precache-manifest.369ebc7ced1d0d4a8229d49e9d3909e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1a95bb3cc4d692425e844288a03649",
+    "url": "/MyFirstApp/index.html"
+  },
+  {
+    "revision": "e8a2b8eda3a6a450694c",
+    "url": "/MyFirstApp/static/css/main.d820349f.chunk.css"
+  },
+  {
+    "revision": "8bf2fdea3eef68e701b9",
+    "url": "/MyFirstApp/static/js/2.3b709a7a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/MyFirstApp/static/js/2.3b709a7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a2b8eda3a6a450694c",
+    "url": "/MyFirstApp/static/js/main.3c3e4378.chunk.js"
+  },
+  {
+    "revision": "814d1c6babdbb7179df9",
+    "url": "/MyFirstApp/static/js/runtime-main.3d55a7f1.js"
+  },
+  {
+    "revision": "107ad235f17173b72bef7996a3d59d3d",
+    "url": "/MyFirstApp/static/media/image.107ad235.png"
+  }
+]);
